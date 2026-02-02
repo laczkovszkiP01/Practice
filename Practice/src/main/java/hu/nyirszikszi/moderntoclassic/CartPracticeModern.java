7c9313f3e351c643ae6e02aa0ebbd985ffa9d48e
laczkovszkiP01 <21c_laczkovszkip@nyirszikszi.hu>
2026-02-02 10:06:59 +0100

    add cartProductNamesUniqeSorted

--- Practice/src/main/java/hu/nyirszikszi/moderntoclassic/CartPracticeModern.java
+++ Practice/src/main/java/hu/nyirszikszi/moderntoclassic/CartPracticeModern.java
@@ -4,6 +4,7 @@ import hu.nyirszikszi.model.Product;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Optional;
 
 public class CartPracticeModern {
@@ -21,6 +22,22 @@ public class CartPracticeModern {
                 .filter(p -> p.getSku() !=null)
                 .filter(p-> p.getSku().trim().toLowerCase(Locale.ROOT).equals(needle))
                 .findFirst();
+    }
+
+    public List<String> cartProductNamesUniqueSorted(List<String> cartSkus){
+        return cartSkus.stream()
+                .filter(Objects::nonNull)
+                .map(String::trim)
+                .filter(s->s.isBlank())
+                .map(this::findBySkuIgnoreCase)
+                .flatMap(Optional::stream)
+                .map(Product::getName)
+                .filter(Objects::nonNull)
+                .map(String::trim)
+                .filter(s-> !s.isBlank())
+                .distinct()
+                .sorted()
+                .toList();
     }
 
 
