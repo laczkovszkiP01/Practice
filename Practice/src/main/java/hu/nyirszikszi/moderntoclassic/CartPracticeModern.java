7936c71f8b9548b61696e417e9e33c5c4d5d1f72
laczkovszkiP01 <21c_laczkovszkip@nyirszikszi.hu>
2026-02-02 10:01:37 +0100

    add findBySkuIgnoreCase

--- Practice/src/main/java/hu/nyirszikszi/moderntoclassic/CartPracticeModern.java
+++ Practice/src/main/java/hu/nyirszikszi/moderntoclassic/CartPracticeModern.java
@@ -3,7 +3,26 @@ package hu.nyirszikszi.moderntoclassic;
 import hu.nyirszikszi.model.Product;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 public class CartPracticeModern {
     private final List<Product> catalog;
+
+    public CartPracticeModern(List<Product> catalog) {
+        this.catalog = catalog;
+    }
+
+    public Optional<Product> findBySkuIgnoreCase(String sku){
+        if (sku == null) return Optional.empty();
+        String needle = sku.trim().toLowerCase(Locale.ROOT);
+
+        return catalog.stream()
+                .filter(p -> p.getSku() !=null)
+                .filter(p-> p.getSku().trim().toLowerCase(Locale.ROOT).equals(needle))
+                .findFirst();
+    }
+
+
+
 }
